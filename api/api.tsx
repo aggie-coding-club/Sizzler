208edc00ca3329816bdd6f3bcf784c69d7781254
Atsubyo <art.tyoung@tamu.edu>
2024-11-14 11:00:20 -0600

    update api routing

--- api/api.tsx
+++ api/api.tsx
@@ -2,11 +2,18 @@ import axios from "axios";
 import { User } from "@/constants/ResponseTypes";
 
 export const getUsers = async (): Promise<User[] | undefined> => {
+	console.log(
+		`http://${
+			process.env.EXPO_PUBLIC_LOCAL_WIFI_HOST ||
+			process.env.EXPO_PUBLIC_TAMU_WIFI_HOST
+		}:${process.env.EXPO_PUBLIC_BACKEND_PORT}/users`
+	);
 	const response = await axios
 		.get<User[]>(
-			`http://${process.env.LOCAL_WIFI_HOST || process.env.TAMU_WIFI_HOST}:${
-				process.env.EXPO_PUBLIC_BACKEND_PORT
-			}/users`,
+			`http://${
+				process.env.EXPO_PUBLIC_LOCAL_WIFI_HOST ||
+				process.env.EXPO_PUBLIC_TAMU_WIFI_HOST
+			}:${process.env.EXPO_PUBLIC_BACKEND_PORT}/users`,
 			{
 				headers: {
 					// Authorization: `Bearer ${}`,
