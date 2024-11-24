81ec8b44870e41aae5e666444d1fc02b134bd82e
Andrew Mao <maosteram@gmail.com>
2024-11-24 17:59:44 -0600

    Login and Signup routing nad auth context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,6 +4,7 @@ import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
+import { AuthProvider } from '@/components/AuthProvider';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
@@ -11,6 +12,8 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+
+
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
@@ -27,13 +30,23 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="auth/log_in" options={{ headerShown: false }} />
-        <Stack.Screen name="auth/sign_up" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <AuthProvider>    
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack 
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: 'rgba(0, 0, 0, 0.8)', // Opaque background
+            },
+            headerTintColor: 'white', // Color for the back button and title
+            headerTitle: '', // Remove the title to only show the back button
+          }}>
+          <Stack.Screen name="index" options={{ headerShown: false}} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="auth/log_in" />
+          <Stack.Screen name="auth/sign_up" />
+          <Stack.Screen name="+not-found" />
+        </Stack>
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
